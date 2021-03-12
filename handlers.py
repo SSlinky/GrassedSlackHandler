b508ad0ac05707ca297b9f78c91f5ddc8cc8827b
Sam Vanderslink <svanderslink@hotmail.com>
2021-03-12 15:48:40 +0800

    close #1

--- handlers.py
+++ handlers.py
@@ -2,14 +2,112 @@ import re
 import requests
 import logging
 
-from logging import Handler, Formatter, LogRecord
+from logging import Handler, LogRecord
+
+
+class Formatter(logging.Formatter):
+    """
+    Formatter instances are used to convert a LogRecord to text.
+
+    Formatters need to know how a LogRecord is constructed. They are
+    responsible for converting a LogRecord to (usually) a string which can
+    be interpreted by either a human or an external system. The base Formatter
+    allows a formatting string to be specified. If none is supplied, the
+    the style-dependent default value, "%(message)s", "{message}", or
+    "${message}", is used.
+
+    The Formatter can be initialized with a format string which makes use of
+    knowledge of the LogRecord attributes - e.g. the default value mentioned
+    above makes use of the fact that the user's message and arguments are pre-
+    formatted into a LogRecord's message attribute. Currently, the useful
+    attributes in a LogRecord are described by:
+
+    %(name)s            Name of the logger (logging channel)
+    %(levelno)s         Numeric logging level for the message (DEBUG, INFO,
+                        WARNING, ERROR, CRITICAL)
+    %(levelname)s       Text logging level for the message ("DEBUG", "INFO",
+                        "WARNING", "ERROR", "CRITICAL")
+    %(pathname)s        Full pathname of the source file where the logging
+                        call was issued (if available)
+    %(filename)s        Filename portion of pathname
+    %(module)s          Module (name portion of filename)
+    %(lineno)d          Source line number where the logging call was issued
+                        (if available)
+    %(funcName)s        Function name
+    %(created)f         Time when the LogRecord was created (time.time()
+                        return value)
+    %(asctime)s         Textual time when the LogRecord was created
+    %(msecs)d           Millisecond portion of the creation time
+    %(relativeCreated)d Time in milliseconds when the LogRecord was created,
+                        relative to the time the logging module was loaded
+                        (typically at application startup time)
+    %(thread)d          Thread ID (if available)
+    %(threadName)s      Thread name (if available)
+    %(process)d         Process ID (if available)
+    %(message)s         The result of record.getMessage(), computed just as
+                        the record is emitted
+    """
+
+    def __init__(self, fmt=None, datefmt=None, style='%', validate=True):
+        """
+        Initialize the formatter with specified format strings.
+
+        Initialize the formatter either with the specified format string, or a
+        default as described above. Allow for specialized date formatting with
+        the optional datefmt argument. If datefmt is omitted, you get an
+        ISO8601-like (or RFC 3339-like) format.
+
+        Use a style parameter of '%', '{' or '$' to specify that you want to
+        use one of %-formatting, :meth:`str.format` (``{}``) formatting or
+        :class:`string.Template` formatting in your format string.
+        """
+        super().__init__(fmt, datefmt, style, validate)
+
+    def format(self, record):
+        """
+        Format the specified record as text.
+
+        The record's attribute dictionary is used as the operand to a
+        string formatting operation which yields the returned string.
+        Before formatting the dictionary, a couple of preparatory steps
+        are carried out. The message attribute of the record is computed
+        using LogRecord.getMessage(). If the formatting string uses the
+        time (as determined by a call to usesTime(), formatTime() is
+        called to format the event time. If there is exception information,
+        it is formatted using formatException() and appended to the message.
+
+        This overrides the base method and removes the exception formatting
+        so that it can be called separately for greater control.
+        """
+        record.message = record.getMessage()
+        if self.usesTime():
+            record.asctime = self.formatTime(record, self.datefmt)
+        return self.formatMessage(record)
+
+    def format_exception(self, record):
+        """
+        Formats the exception message, if there is one, as a code block.
+        """
+        s = '```'
+        if record.exc_info:
+            # Cache the traceback text to avoid converting it multiple times
+            # (it's constant anyway)
+            if not record.exc_text:
+                record.exc_text = self.formatException(record.exc_info)
+        if record.exc_text:
+            s = s + record.exc_text
+        if record.stack_info:
+            if s[-1:] != "\n":
+                s = s + "\n"
+            s = s + self.formatStack(record.stack_info)
+        return s + '\n```'
 
 
 class SlackHandler(Handler):
     """
     Handler instances dispatch logging events to specific destinations.
 
-    The Slack handler class. Builds on the base handler to emit formatted
+    The Slack handler class builds on the base handler to emit formatted
     log records to a Slack channel.
     """
     def __init__(self, url, fmt=None, datefmt=None, style='%'):
@@ -31,6 +129,7 @@ class SlackHandler(Handler):
         """Builds a json payload that the Slack API can process"""
         blocks = []
 
+        # Generate the main layout blocks
         for name, fmt in self.formats:
             if name in self.tags:
                 blocks.append(
@@ -41,6 +140,16 @@ class SlackHandler(Handler):
                 t = ", ".join(self.tags)
                 raise ValueError(
                     f'{name} not a valid tag name. Must be in [{t}]')
+
+        # Generate the exception block if there is one
+        if record.exc_info:
+            fmt = Formatter(fmt='')
+            blocks.append(
+                self.__gen_layout_block(
+                    'section', fmt.format_exception(record)
+                )
+            )
+
         return blocks
 
     def setFormatter(self, fmt: Formatter) -> None:
